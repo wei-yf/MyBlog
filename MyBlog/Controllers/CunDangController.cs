12e9bd1e3fb812b3c36434ac5e0c0527d6e8fdca
位亚飞 <wyf2012@outlook.com>
2017-07-15 17:34:26 +0800

    归档准备

---
+++ MyBlog/Controllers/CunDangController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyBlog.Controllers
+{
+    public class CunDangController : Controller
+    {
+        // GET: CunDang
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult tes()
+        {
+            return View();
+        }
+    }
+}
