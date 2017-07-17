d26ea3ecb8d18795785bf3daf8c0345e53ddfa0f
位亚飞 <wyf2012@outlook.com>
2017-07-17 16:20:50 +0800

    归档

--- MyBlog/Controllers/CunDangController.cs
+++ MyBlog/Controllers/CunDangController.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MyBlog.Models;
+using System.Data;
+using MyBlog.Help;
+using Dapper;
+using MyBlog.Areas.MyBlog.Models;
 
 namespace MyBlog.Controllers
 {
@@ -11,7 +16,23 @@ namespace MyBlog.Controllers
         // GET: CunDang
         public ActionResult Index()
         {
-            return View();
+            CunDangClass cd = new CunDangClass();
+            using (IDbConnection conn = DapperHelp.GetOpenConnection())
+            {
+                string sql = "select SUBSTRING(date,1,4) as year from Article group by SUBSTRING(date,1,4) order by SUBSTRING(date,1,4) desc";
+                cd.YearList = conn.Query<YearClass>(sql).ToList();
+                foreach(var v in cd.YearList)
+                {
+                    string sql2 = "select SUBSTRING(date,6,2) as month from Article where SUBSTRING(date,1,4)=@year group by SUBSTRING(date, 1, 7),SUBSTRING(date, 6, 2) ,SUBSTRING(date, 1, 4)order by SUBSTRING(date, 1, 7) desc";
+                    v.MonthList = conn.Query<MonthClass>(sql2, new { year = v.Year }).ToList();
+                    foreach(var m in v.MonthList)
+                    {
+                        string sql3 = "select * from Article where SUBSTRING(date,1,7)=@date";
+                        m.ArticleList = conn.Query<Article>(sql3, new { date = v.Year + "-" + m.Month }).ToList();
+                    }
+                }
+            }
+                return View(cd);
         }
         public ActionResult tes()
         {
---
+++ MyBlog/Models/CunDangClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MyBlog.Areas.MyBlog.Models;
+
+namespace MyBlog.Models
+{
+    public class CunDangClass
+    {
+        public List<YearClass> YearList { get; set; }
+    }
+
+    public class YearClass
+    {
+        public string Year { get; set; }
+        public List<MonthClass> MonthList { get; set; }
+    }
+    public class MonthClass
+    {
+        public string Month { get; set; }
+        public List<Article> ArticleList { get; set; }
+    }
+}
