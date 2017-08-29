0be9a2044721c80ee6e56dc75a7d6a8d4661415f
位亚飞 <wyf2012@outlook.com>
2017-08-29 08:53:38 +0800

    天气图片样式

--- MyBlog/Help/RequestHelp.cs
+++ MyBlog/Help/RequestHelp.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Text;
 using System.Web;
 
 namespace MyBlog.Help
@@ -11,22 +12,7 @@ namespace MyBlog.Help
     {
         public static string getreq(string url, Dictionary<string, string> para)
         {
-            if(para.Count>0)
-            {
-                url = url + "?";
-                int n = 0;
-                foreach(var item in para)
-                {
-                    if (n == 0)
-                    {
-                        url += item.Key + "=" + item.Value;
-                    }else
-                    {
-                        url += "&" + item.Key + "=" + item.Value;
-                    }
-                    n++;
-                }
-            }
+            url = buildUrl(url, para);
             HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
             req.Method = "GET";
             req.ContentType = "application/x-www-form-urlencoded";
@@ -36,6 +22,44 @@ namespace MyBlog.Help
             sr.Close();
             res.Close();
             return backstr;
+        }
+        public static string postreq(string url, Dictionary<string, string> para, string postStr)
+        {
+            url = buildUrl(url, para);
+            HttpWebRequest req= (HttpWebRequest)WebRequest.Create(url);
+            req.Method = "POST";
+            req.ContentType = "application/x-www-form-urlencoded";
+            req.ContentLength = Encoding.UTF8.GetByteCount("");
+            byte[] bytes = Encoding.UTF8.GetBytes(postStr);
+            var reqStream = req.GetRequestStream();
+            reqStream.Write(bytes, 0, bytes.Length);
+            reqStream.Close();
+
+            var respStream = req.GetResponse().GetResponseStream();
+            StreamReader sr = new StreamReader(respStream);
+
+            return sr.ReadToEnd();
+        }
+        public static string buildUrl(string url, Dictionary<string, string> para)
+        {
+            if (para.Count > 0)
+            {
+                url = url + "?";
+                int n = 0;
+                foreach (var item in para)
+                {
+                    if (n == 0)
+                    {
+                        url += item.Key + "=" + item.Value;
+                    }
+                    else
+                    {
+                        url += "&" + item.Key + "=" + item.Value;
+                    }
+                    n++;
+                }
+            }
+            return url;
         }
     }
 }
