3ecbb356f301ca5d35aad735a4b4bddd8be8d3d3
位亚飞 <wyf2012@outlook.com>
2017-09-05 10:01:13 +0800

    归档bug

--- MyBlog/Controllers/CunDangController.cs
+++ MyBlog/Controllers/CunDangController.cs
@@ -19,15 +19,15 @@ namespace MyBlog.Controllers
             CunDangClass cd = new CunDangClass();
             using (IDbConnection conn = DapperHelp.GetOpenConnection())
             {
-                string sql = "select SUBSTRING(date,1,4) as year from Article group by SUBSTRING(date,1,4) order by SUBSTRING(date,1,4) desc";
+                string sql = "select SUBSTRING(date,1,4) as year from Article where DeletedOn is null group by SUBSTRING(date,1,4),DeletedOn order by SUBSTRING(date,1,4) desc";
                 cd.YearList = conn.Query<YearClass>(sql).ToList();
                 foreach(var v in cd.YearList)
                 {
-                    string sql2 = "select SUBSTRING(date,6,2) as month from Article where SUBSTRING(date,1,4)=@year group by SUBSTRING(date, 1, 7),SUBSTRING(date, 6, 2) ,SUBSTRING(date, 1, 4)order by SUBSTRING(date, 1, 7) desc";
+                    string sql2 = "select SUBSTRING(date,6,2) as month from Article where SUBSTRING(date,1,4)=@year and DeletedOn is null group by SUBSTRING(date, 1, 7),SUBSTRING(date, 6, 2) ,SUBSTRING(date, 1, 4) ,DeletedOn order by SUBSTRING(date, 1, 7) desc";
                     v.MonthList = conn.Query<MonthClass>(sql2, new { year = v.Year }).ToList();
                     foreach(var m in v.MonthList)
                     {
-                        string sql3 = "select * from Article where SUBSTRING(date,1,7)=@date";
+                        string sql3 = "select * from Article where SUBSTRING(date,1,7)=@date and DeletedOn is null";
                         m.ArticleList = conn.Query<Article>(sql3, new { date = v.Year + "-" + m.Month }).ToList();
                     }
                 }
